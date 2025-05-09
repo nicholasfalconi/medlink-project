aa5102cd3c866d4658d2b73f16e1225a8bbe9a1d
Zulfa Varvani <69704045+Zulfa-Varvani@users.noreply.github.com>
2025-05-09 17:19:33 +0000

    add files zulfa

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from "path";
 
 export default defineConfig(({ mode }) => ({
   // Using relative path for GitHub Pages deployment
-  base: mode === 'production' ? '/medlink-project/' : '/',
+  base: '/',
   server: {
     host: "::",
     port: 8080,
